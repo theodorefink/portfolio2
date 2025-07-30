a8f5366bad298c54ae28c0518b9e19a4aeb0892a
finth628 <finth628@student.otago.ac.nz>
2025-07-30 16:49:41 +1200

    update user id and client id for SoundCloud API

--- api/soundcloud.js
+++ api/soundcloud.js
@@ -1,5 +1,5 @@
  export default async function handler(req, res) {
-    const client_id = 'T26Olo5VaFwfbJtWjYtvIFx3vOe4v84D';
+    const client_id = 'UDiAYmFAk8V7GK8c6yXswhPecbExhiYk';
     const { path } = req.query;
   
     const fullUrl = `https://api-v2.soundcloud.com/${path}?client_id=${client_id}`;
