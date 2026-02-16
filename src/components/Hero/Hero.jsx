e75a89218312b9f3c180a033e279d5a3df8eeb72
finth628 <finth628@student.otago.ac.nz>
2026-02-17 09:15:45 +1300

    Edit hero msg

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,31 +1,39 @@
 import React from "react";
-import styles from './Hero.module.css';
-import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
-import profilePic from '../../assets/profilepic.jpg'; 
+import styles from "./Hero.module.css";
+import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
+import profilePic from "../../assets/profilepic.jpg";
 
 export const Hero = () => {
   return (
     <section className={styles.hero}>
       <div className={styles.introduction}>
         <div className={styles.profileImageContainer}>
-          <img 
-            src={profilePic} 
-            alt="Photo of me" 
-            className={styles.profileImage} 
+          <img
+            src={profilePic}
+            alt="Photo of me"
+            className={styles.profileImage}
           />
         </div>
-        <h1>Theo Fink</h1> 
+        <h1>Theo Fink</h1>
       </div>
 
       <div className={styles.contact}>
         <ul className={styles.contactList}>
           <li>
-            <a href="https://github.com/theodorefink" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://github.com/theodorefink"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <FaGithub className={styles.icon} />
             </a>
           </li>
           <li>
-            <a href="https://www.linkedin.com/in/theodore-fink/" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://www.linkedin.com/in/theodore-fink/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <FaLinkedin className={styles.icon} />
             </a>
           </li>
@@ -38,7 +46,13 @@ export const Hero = () => {
       </div>
 
       <div className={styles.blurb}>
-        <p>Kia ora! I'm Theo, a third year computer science student at the University of Otago. Outside of uni, I enjoy working on some personal coding projects, as well as my other hobbies. Navigate through this website to learn more about me, my experience in software development and my projects!</p>
+        <p>
+          Kia ora! I'm Theo, a fourth year computer science student studying my honours at the
+          University of Canterbury. Outside of uni, I enjoy working on some personal
+          coding projects, as well as my other hobbies. Navigate through this
+          website to learn more about me, my experience in software development
+          and my projects!
+        </p>
       </div>
 
       {/* <div className={styles.previewPane}>
